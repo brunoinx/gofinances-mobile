b25f8e1b4a4b2dabd799525294233de55fde779e
Bruno Henrique <brunoh.henriqueh01@gmail.com>
2022-01-25 15:11:14 -0300

    feat: create user info wrapper in header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useTheme } from 'styled-components'
 
 import * as S from './styles'
 
 export function Dashboard() {
+  const theme = useTheme()
+
   return (
     <S.Container>
-      <S.Header />
+      <S.Header>
+        <S.HeaderTop>
+          <S.UserInfo>
+            <S.Photo source={{ uri: 'https://www.github.com/brunoinx.png' }} />
+
+            <S.User>
+              <S.UserGretting>Olá, </S.UserGretting>
+              <S.UserName>Bruno Henrique</S.UserName>
+            </S.User>
+          </S.UserInfo>
+
+          <S.Logout onPress={() => ''}>
+            <S.Icon name="power" size={24} color={theme.colors.attention} />
+          </S.Logout>
+        </S.HeaderTop>
+      </S.Header>
     </S.Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,66 @@
 import styled, { css } from 'styled-components/native'
+import { RFPercentage } from 'react-native-responsive-fontsize'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { Feather } from '@expo/vector-icons'
 
 export const Container = styled.View`
   ${({ theme }) => css`
     flex: 1;
-    background-color: ${theme.colors.secondary};
+    background-color: ${theme.colors.background};
   `}
 `
 
 export const Header = styled.View`
   ${({ theme }) => css`
     width: 100%;
+    height: ${RFPercentage(40)}px;
     background-color: ${theme.colors.primary};
+
+    padding-top: ${getStatusBarHeight() + RFPercentage(3)}px;
   `}
+`
+
+export const HeaderTop = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 ${RFPercentage(4)}px;
+  margin-bottom: ${RFPercentage(3)}px;
+`
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+`
+
+export const Photo = styled.Image`
+  width: ${RFPercentage(7)}px;
+  height: ${RFPercentage(7)}px;
+  border-radius: ${RFPercentage(1)}px;
+`
+
+export const User = styled.View`
+  margin-left: ${RFPercentage(2)}px;
+`
+
+export const UserGretting = styled.Text`
+  font-size: ${RFPercentage(2.5)}px;
+  font-family: ${({ theme }) => theme.fonts.text_400};
+  color: ${({ theme }) => theme.colors.background};
+`
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.background};
+  font-size: ${RFPercentage(2.5)}px;
+  font-family: ${({ theme }) => theme.fonts.text_700};
+`
+
+export const Logout = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})``
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFPercentage(3.5)}px;
+  margin-left: ${RFPercentage(3)}px;
 `
