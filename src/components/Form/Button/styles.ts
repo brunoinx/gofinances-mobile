0fd5b6fba723baa9dcfe7eca80d8ccb6c8528a8c
Bruno Henrique <brunoh.henriqueh01@gmail.com>
2022-01-30 20:50:42 -0300

    creating button component

---
+++ src/components/Form/Button/styles.ts
@@ -0,0 +1,21 @@
+import styled, { css } from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+export const Container = styled(RectButton)`
+  ${({ theme }) => css`
+    width: 100%;
+    background: ${theme.colors.secondary};
+    padding: 18px;
+    border-radius: 5px;
+    align-items: center;
+  `}
+`
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.text_500};
+    font-size: ${RFValue(15)}px;
+    color: ${theme.colors.shape};
+  `}
+`
