d300cd99fa0ab3c1dea5c0b58a40ceba357997a6
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-08-12 22:19:22 -0300

    feat: :sparkles: create signin page

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import {
+  ANDROID_GOOGLE_CLIENT_ID,
+  EXPO_CLIENT_ID,
+  IOS_GOOGLE_CLIENT_ID,
+} from '@env';
+
+import { useAuth } from '@/hooks/useAuth';
+import { getUserStorage } from '@/storage/user';
+import { SignInSocialButton } from '@/components/SignInSocialButton';
+
+import LogoSvg from '@/assets/logo.svg';
+import * as S from './styles';
+import { requestGoogleUserData } from '@/services/requestGoogleUserData';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export function SignIn() {
+  const { handleUpdateUserData, handleSocialAuthentication } = useAuth();
+  const [_, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: EXPO_CLIENT_ID,
+    androidClientId: ANDROID_GOOGLE_CLIENT_ID,
+    iosClientId: IOS_GOOGLE_CLIENT_ID,
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication) {
+      handleSignInWithGoogle();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [response]);
+
+  async function handleSignInWithGoogle() {
+    try {
+      const userStorage = await getUserStorage();
+
+      if (!userStorage) {
+        if (response?.type === 'success') {
+          const user = await requestGoogleUserData(
+            response.authentication.accessToken,
+          );
+
+          handleSocialAuthentication(user);
+        }
+      } else {
+        handleUpdateUserData(userStorage);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+
+          <S.Title>
+            Controle suas {'\n'}
+            finanças de forma {'\n'}
+            muito simples
+          </S.Title>
+        </S.TitleWrapper>
+
+        <S.SignInTitle>
+          Faça seu login com {'\n'}
+          uma das contas abaixo
+        </S.SignInTitle>
+      </S.Header>
+
+      <S.Footer>
+        <S.FooterWrapper>
+          <SignInSocialButton
+            title="Entrar com Google"
+            iconName="google"
+            onPress={() => promptAsync()}
+          />
+
+          {Platform.OS === 'ios' && (
+            <SignInSocialButton
+              title="Entrar com Apple"
+              iconName="apple"
+              onPress={() => {}}
+            />
+          )}
+        </S.FooterWrapper>
+      </S.Footer>
+    </S.Container>
+  );
+}
