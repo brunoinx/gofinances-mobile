f56e1d38d6ffe81030bd34bffa2b4fa6a790d15c
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2023-08-12 22:17:55 -0300

    feat: :sparkles: create switch auth and app routes when authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
+
 import { AppRoutes } from './app.routes';
+import { AuthRoutes } from './auth.routes';
+
+import { useAuth } from '@/hooks/useAuth';
 
 export function Routes() {
+  const { isUserAuthenticated } = useAuth();
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {!isUserAuthenticated ? <AuthRoutes /> : <AppRoutes />}
     </NavigationContainer>
   );
 }
